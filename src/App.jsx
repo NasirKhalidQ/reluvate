415616751fb443db6858c4bd29ae948ebdc5d834
Nasir Khalid <nasir.khalid.new@gmail.com>
2022-09-26 20:36:39 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-tile">
+      <div className="flex w-full bg-black p-4">
+        <h2 className="text-3xl text-white">Catch 'em all</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
