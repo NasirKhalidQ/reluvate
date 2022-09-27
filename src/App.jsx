ebdb7742c5247c22f155ca25a295dfe4289ec0cc
Nasir Khalid <nasir.khalid.new@gmail.com>
2022-09-28 00:01:37 +0500

    added CatchPokemon component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
 import { useState } from "react";
-import { Pokemons } from "./pokemon";
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { CatchPokemon } from "./components/CatchPokemon/CatchPokemon";
 
 function App() {
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    setMagicNumber(Math.floor(Math.random() * (max - min + 1) + min)); // The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
   }
 
-  const [guess, setGuess] = useState(0);
-  const [magicNumber, setMagicNumber] = useState(0);
-  const [tries, setTries] = useState(3);
+  const [state, setState] = useState({
+    guess: 0,
+    magicNumber: 0,
+    tries: 3,
+    currentPokemon: 0,
+  });
 
-  const handleSubmit = () => {
-    const mNumber = parseInt(magicNumber);
-    const guessNumber = parseInt(guess);
-
-    if (mNumber === guessNumber) {
-      toast.success("Congrats! You caught a Pokemon");
-    } else {
-      if (guessNumber > mNumber) {
-        toast.error(
-          `Incorrect! Your number is too high\nYou have ${tries} tries left`
-        );
-      } else {
-        toast.error(
-          `Incorrect! Your number is too low\nYou have ${tries} tries left`
-        );
-      }
-    }
+  const startGame = () => {
+    const currentPokemon = parseInt(getRandomIntInclusive(0, 15));
+    const magicNumber = parseInt(getRandomIntInclusive(1, 10));
+    setState({ ...state, currentPokemon, magicNumber });
   };
 
   return (
@@ -38,49 +27,13 @@ function App() {
       <div className="flex w-full bg-black p-4 justify-between">
         <h2 className="text-3xl text-white">Catch 'em all</h2>
         <div className="flex gap-x-4">
-          <button
-            onClick={() => getRandomIntInclusive(1, 10)}
-            className="bg-white"
-          >
+          <button onClick={() => startGame()} className="bg-white">
             Catch Pokemon
           </button>
           <button className="bg-white">View Pokemon</button>
         </div>
       </div>
-      <div className="flex flex-col gap-y-5 place-items-center mt-6 font-semibold ">
-        <h2 className="text-redFa">Want to catch this Pokemon?</h2>
-        <div
-          className={`w-72 h-96 p-10 border-black border-4 rounded-2xl shadow-2xl transition ease-linear delay-300 hover:scale-110 ${Pokemons[15].shadow} ${Pokemons[15].bg}`}
-        >
-          <img src={Pokemons[15].imageUrl} alt={Pokemons[2].name} />
-        </div>
-        <h4 className="mt-4 font-medium">Pick a number between 1 and 10</h4>
-        <input
-          className="px-3 py-2 w-64 rounded-sm focus:outline-gray-900 bg-gray-100 border-2 border-zinc-500"
-          type="number"
-          min={0}
-          onChange={(e) => setGuess(e.currentTarget.value)}
-        />
-        <button
-          onClick={handleSubmit}
-          className="bg-black w-64 p-2 text-white rounded-sm hover:text-redFa"
-        >
-          Submit
-        </button>
-        <h4>
-          {parseInt(magicNumber) === parseInt(guess) ? "Success" : "Fails"}
-        </h4>
-        <ToastContainer
-          position="bottom-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-        />
-      </div>
+      <CatchPokemon state={state} setState={setState} />
     </div>
   );
 }
---
+++ src/components/CatchPokemon/CatchPokemon.jsx
@@ -0,0 +1,71 @@
+import { ToastContainer, toast } from "react-toastify";
+import { Pokemons } from "../../pokemon";
+
+export const CatchPokemon = ({ state, setState }) => {
+  const handleSubmit = () => {
+    const mNumber = parseInt(state.magicNumber);
+    const guessNumber = parseInt(state.guess);
+    if (state.tries > 0) {
+      setState({ ...state, tries: state.tries - 1 });
+    }
+
+    if (mNumber === guessNumber) {
+      toast.success("Congrats! You caught a Pokemon");
+    } else {
+      if (guessNumber > mNumber) {
+        toast.error(
+          `Incorrect! Your number is too high. You have ${state.tries} tries left`
+        );
+      } else {
+        toast.error(
+          `Incorrect! Your number is too low. You have ${state.tries} tries left`
+        );
+      }
+    }
+  };
+  return (
+    <div className="flex flex-col gap-y-5 place-items-center mt-6 font-semibold ">
+      <h2 className="text-redFa">Want to catch this Pokemon?</h2>
+      <div
+        className={`w-72 h-96 px-10 py-3 text-black border-black border-4 flex flex-col rounded-2xl shadow-2xl transition ease-linear delay-300 hover:scale-110 ${
+          Pokemons[state.currentPokemon].shadow
+        } ${Pokemons[state.currentPokemon].bg}`}
+      >
+        <p className="ml-16 text-xl">{`#00${state.currentPokemon + 1}`}</p>
+        <img
+          src={Pokemons[state.currentPokemon].imageUrl}
+          alt={Pokemons[state.currentPokemon].name}
+        />
+        <p className="mt-4">Name: {Pokemons[state.currentPokemon].name}</p>
+        <p>HP: {Pokemons[state.currentPokemon].hp}</p>
+        <p>Attack: {Pokemons[state.currentPokemon].attack}</p>
+        <p>Defense: {Pokemons[state.currentPokemon].defense}</p>{" "}
+        <p>Type: {Pokemons[state.currentPokemon].type}</p>
+      </div>
+      <h4 className="mt-4 font-medium">Pick a number between 1 and 10</h4>
+      <input
+        className="px-3 py-2 w-64 rounded-sm focus:outline-gray-900 bg-gray-100 border-2 border-zinc-500"
+        type="number"
+        min={0}
+        onChange={(e) => setState({ ...state, guess: e.currentTarget.value })}
+      />
+      <button
+        onClick={handleSubmit}
+        className="bg-black w-64 p-2 text-white rounded-sm hover:text-redFa transition-all ease-in-out duration-300"
+      >
+        Submit
+      </button>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+      />
+    </div>
+  );
+};
