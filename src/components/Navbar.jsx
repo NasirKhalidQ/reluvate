3354d0590b4f56a1a81171178b7a21db41a805ea
Nasir Khalid <nasir.khalid.new@gmail.com>
2022-09-28 18:59:26 +0500

    styled buttons in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,20 @@
 export const Navbar = ({ startGame }) => {
+  const Button = ({ onClick, children }) => {
+    return (
+      <button
+        onClick={onClick}
+        className="bg-black border-redFa border-2 p-2 text-white font-medium rounded-sm hover:text-redFa transition-all ease-in-out duration-300"
+      >
+        {children}
+      </button>
+    );
+  };
   return (
     <div className="flex w-full bg-black p-4 justify-between">
       <h2 className="text-3xl text-white">Catch 'em all</h2>
       <div className="flex gap-x-4">
-        <button onClick={() => startGame()} className="bg-white">
-          Catch Pokemon
-        </button>
-        <button className="bg-white">View Pokemon</button>
+        <Button onClick={() => startGame()}>Catch Pokemon</Button>
+        <Button onClick={() => startGame()}> View Pokemon</Button>
       </div>
     </div>
   );
