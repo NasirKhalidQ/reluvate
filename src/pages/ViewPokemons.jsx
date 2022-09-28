ab133e60d998511bf71a78e372ea98c1db0b9e42
Nasir Khalid <nasir.khalid.new@gmail.com>
2022-09-29 01:21:31 +0500

    added pokemon favicon svg

--- src/pages/ViewPokemons.jsx
+++ src/pages/ViewPokemons.jsx
@@ -5,7 +5,8 @@ export const ViewPokemons = ({ pokemonList, deletePokemon }) => {
     <div className="flex flex-col gap-y-10 mt-6 font-semibold p-4">
       {pokemonList?.length > 0 ? (
         <h2 className="text-green-600">
-          You possess {pokemonList.length} Pokemon(s)! You are rich!
+          You possess {pokemonList.length} Pokemon
+          {pokemonList.length > 1 ? "s" : ""}! You are rich!
         </h2>
       ) : (
         <div className="flex -gap-x-4">
