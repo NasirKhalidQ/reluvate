34716d6ebe4124f5d83085d63fd619b55428f267
Nasir Khalid <nasir.khalid.new@gmail.com>
2022-10-01 23:01:15 +0500

    removed state from login to use refs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,11 @@
-export const Input = ({ type, value, setState }) => {
+import { forwardRef } from "react";
+
+export const Input = forwardRef((props, ref) => {
   return (
     <input
       className="px-3 py-2 w-64 rounded-sm focus:outline-black border-2 border-black"
-      type={type}
-      value={value}
-      onChange={(e) => setState(e.target.value)}
+      ref={ref}
+      type={props.type}
     />
   );
-};
+});
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Button, Input } from "../components";
 export const Login = () => {
   const navigate = useNavigate();
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = emailRef?.current?.value;
+    const password = passwordRef?.current?.value;
     if (email && password) {
       toast.success("Successful login");
       setTimeout(() => {
@@ -26,17 +28,20 @@ export const Login = () => {
         <h1 className="text-4xl text-redFa font-medium">Catch 'em All</h1>
         <div className="flex flex-col gap-y-12 place-items-center justify-center  border-2 border-black py-10 px-8 rounded">
           <h1>Login</h1>
-          <div className="flex flex-col gap-y-4 font-medium text-lg">
+          <form
+            className="flex flex-col gap-y-4 font-medium text-lg"
+            onSubmit={(e) => handleLogin(e)}
+          >
             <div>
               <p>Email</p>
-              <Input type="text" value={email} setState={setEmail} />
+              <Input type="text" ref={emailRef} />
             </div>
             <div>
               <p>Password</p>
-              <Input type="password" value={password} setState={setPassword} />
+              <Input type="password" ref={passwordRef} />
             </div>
-            <Button handleClick={handleLogin}>Login</Button>
-          </div>
+            <Button handleClick={undefined}>Login</Button>
+          </form>
         </div>
       </div>
       <ToastContainer
